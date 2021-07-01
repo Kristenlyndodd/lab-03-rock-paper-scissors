126bd8960aa88523918837e183e96e2d819955f0
kristenlynodd <kristenlyndodd@gmail.com>
2021-07-01 16:47:22 -0700

    DOM elements, import functions, initialized state

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // import functions and grab DOM elements
+import { didUserWin } from './utils.js';
+
+const playButton = document.getElementById('play');
+const winsSpan = document.getElementById('total-wins');
+const lossesSpan = document.getElementById('total-losses');
 
 // initialize state
+let wins = 0;
+let losses = 0;
+let randomNum = Math.ceil(Math.random() * 3);
+
+console.log(didUserWin, winsSpan, lossesSpan, wins, losses, randomNum);
 
 // set event listeners 
+playButton.addEventListener('click', ()=>{
+
+    // const selected = document.querySelector('input[type=radio]:checked');
+    // const userChoice = selected.value;
+
+    // let computerChoice = 'rock';
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ utils.js
@@ -0,0 +1,20 @@
+export function didUserWin(player, computer) {
+    console.log(player, computer);
+    
+    // player
+    // rock, paper, scissors
+    // 
+    // computer 
+    // rock paper scissors
+    // 
+    // if the userThrow is the same as the computerThrow
+    //   return 'draw'
+    // if the user throws rock
+    //    if computerThrow is scissors return 'win'
+    // if the user throws paper
+    //    if computerThrow is rock return 'win'
+    // if the user throw scissors
+    //    if computerThrow is paper return 'win'
+    // return 'lose'    
+    
+}
